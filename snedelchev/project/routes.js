b6a6eefffb5ed9416ce9a1fc4c1df1044de6352e
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2022-01-28 12:06:51 +0200

    Added new method - GET /sequence/gene/:id

--- snedelchev/project/routes.js
+++ snedelchev/project/routes.js
@@ -1,13 +1,31 @@
 import express from 'express';
+import axios from 'axios';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const result = {
-        message: 'success!'
-    };
+router.get('/sequence/gene/:id', async (req, res) => {
+    const id = req.params.id; // ENSG00000157764
 
-    res.json(result);
+    const seqResult = await axios.get(`https://rest.ensembl.org/sequence/id/${id}`);
+    const { seq } = seqResult.data;
+
+    const exonsResult = (await axios.get(`https://rest.ensembl.org/lookup/id/${id}?expand=1`))
+        .data
+        .Transcript[0]
+        .Exon;
+
+    const exons = exonsResult.map(e => {
+        return {
+            start: e.start,
+            end: e.end,
+            id: e.id
+        };
+    });
+
+    res.send({
+        seq,
+        exons
+    });
 });
 
 export { router };
