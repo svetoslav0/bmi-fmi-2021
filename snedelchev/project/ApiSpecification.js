14d1e800aa989d87ccf8e7e4ddc8be1040e1125f
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2022-01-31 00:25:25 +0200

    Updated API Specification

--- snedelchev/project/ApiSpecification.js
+++ snedelchev/project/ApiSpecification.js
@@ -1,5 +1,5 @@
 const APPLICATION_VERSION = '1.0.0';
-const CONTACT_EMAIL = 'svet.nedelchev@gmail.com';
+const DEV_CONTACT_EMAIL = 'svet.nedelchev@gmail.com';
 const APPLICATION_HOST = 'localhost:9091'; // TODO: get the host from config
 
 export const ApiSpecification = () => {
@@ -10,7 +10,7 @@ export const ApiSpecification = () => {
             version: APPLICATION_VERSION,
             title: 'TODO',
             contact: {
-                email: CONTACT_EMAIL
+                email: DEV_CONTACT_EMAIL
             }
         },
         host: APPLICATION_HOST,
@@ -18,10 +18,6 @@ export const ApiSpecification = () => {
             {
                 name: 'Gene',
                 description: 'Gene Related Paths'
-            },
-            {
-                name: 'Sequence',
-                description: 'Sequence Related Paths'
             }
         ],
         paths: {
@@ -49,7 +45,53 @@ export const ApiSpecification = () => {
                             schema: {
                                 $ref: '#/definitions/GeneData'
                             }
-                        } // TODO: Add 400 response schema
+                        },
+                        400: {
+                            description: 'Bad Request',
+                            schema: {
+                                $ref: '#/definitions/BadRequest'
+                            }
+                        }
+                    }
+                }
+            },
+            '/sequence/{id}/gc_content': {
+                get: {
+                    tags: [
+                        'Sequence'
+                    ],
+                    summary: 'Get GC content and swapped bases',
+                    description: 'This method returns GC content (percentage content of G and C bases in the sequence) and swaps two bases by provided bases in the swap parameter',
+                    operationId: 'getGcContentData',
+                    parameters: [
+                        {
+                            name: 'id',
+                            in: 'path',
+                            description: 'The ID of the gene',
+                            required: true,
+                            type: 'string'
+                        },
+                        {
+                            name: 'swap',
+                            in: 'query',
+                            description: 'Swap parameter. Pass the two bases that need to be swapped in the following format: {base-one}:{base-two}. Examples: A:T, G:C, A:C',
+                            required: false,
+                            type: 'string'
+                        }
+                    ],
+                    responses: {
+                        200: {
+                            description: 'GC content data',
+                            schema: {
+                                $ref: '#/definitions/GCData'
+                            }
+                        },
+                        400: {
+                            description: 'Bad Request',
+                            schema: {
+                                $ref: '#/definitions/BadRequest'
+                            }
+                        }
                     }
                 }
             },
@@ -76,7 +118,13 @@ export const ApiSpecification = () => {
                             schema: {
                                 $ref: '#/definitions/SequencesData'
                             }
-                        } // TODO: Add 400 response schema
+                        },
+                        400: {
+                            description: 'Bad Request',
+                            schema: {
+                                $ref: '#/definitions/BadRequest'
+                            }
+                        }
                     }
                 }
             }
@@ -115,6 +163,24 @@ export const ApiSpecification = () => {
                     }
                 }
             },
+            GCData: {
+                type: 'object',
+                description: 'GC and swapped data',
+                properties: {
+                    seq: {
+                        type: 'string',
+                        description: 'The sequence by the provided ID'
+                    },
+                    gc_content: {
+                        type: 'integer',
+                        description: 'Percentage of G and C bases'
+                    },
+                    swap_seq: {
+                        type: 'string',
+                        description: 'Swapped sequence by given swap bases. If swap parameter is not provided, the value must be the same as the seq field'
+                    }
+                }
+            },
             SequencesData: {
                 type: 'array',
                 description: 'List of sequences',
@@ -132,6 +198,15 @@ export const ApiSpecification = () => {
                     seq: {
                         type: 'string',
                         description: 'The sequence itself'
+                    }
+                }
+            },
+            BadRequest: {
+                type: 'object',
+                properties: {
+                    message: {
+                        type: 'string',
+                        description: 'Message with description of the issue'
                     }
                 }
             }
