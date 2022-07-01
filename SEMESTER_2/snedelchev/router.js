741352fe8bfcee4f79e4defc54a0a4affe393a76
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2022-07-01 12:44:47 +0300

    Added methods for adding and getting exposures

--- SEMESTER_2/snedelchev/router.js
+++ SEMESTER_2/snedelchev/router.js
@@ -1,16 +1,59 @@
 import express from "express";
 
 import { getDb } from "./database.js";
+import { ExposureFormatBuilder } from './helpers/ExposureFormatBuilder.js';
 import { DataFormatBuilder } from "./helpers/DataFormatBuilder.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
 
+router.post('/exposures', (req, res) => {
+    const data = ExposureFormatBuilder.formatCreate(req.body);
+
+    getDb()
+        .collection('exposures')
+        .insertOne(data, (error, result) => {
+            if (error) {
+                console.error(error);
+            }
+
+            data.exposure_id = data._id.toString();
+            const updated = { $set: data };
+            getDb()
+                .collection('exposures')
+                .updateOne({ _id: new ObjectId(data.exposure_id) }, updated, err => {
+                if (err) {
+                    console.error(err);
+                }
+
+                res.status(201).json({ success: true });
+            });
+        });
+});
+
+router.get('/exposures/:id', (req, res) => {
+    const id = req.params.id;
+
+    getDb()
+        .collection('exposures')
+        .findOne({ _id: new ObjectId(id) }, (err, result) => {
+            if (err) {
+                console.error(err);
+            }
+
+            if (!result) {
+                return res.status(404).json({ message: 'Not found' });
+            }
+
+            res.json(result);
+        });
+});
+
 router.post('/', (req, res) => {
-    const databaseConnection = getDb();
     const data = DataFormatBuilder.formatAddData(req.body);
 
-    databaseConnection
+    getDb()
         .collection('clinical_data')
         .insertOne(data, (error, result) => {
             if (error) {
