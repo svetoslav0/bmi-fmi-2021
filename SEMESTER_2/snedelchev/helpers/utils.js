a3e95c52dbdf38c60febc0680c61aec521f41344
Svetoslav Nedelchev <snedelchev@isimarkets.com>
2022-07-08 19:35:15 +0300

    Removed old project

--- SEMESTER_2/snedelchev/helpers/utils.js
+++
@@ -1,7 +0,0 @@
-export function removeUndefinedProps(obj) {
-    for (let prop in obj) {
-        if (obj.hasOwnProperty(prop) && (obj[prop] === undefined || obj[prop] === null)) {
-            delete obj[prop];
-        }
-    }
-}
